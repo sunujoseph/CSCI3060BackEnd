b37e87d41113ff5554a8c7977802fab7134884dc
Tilova Shahrin <46762829+tilovashahrin@users.noreply.github.com>
2020-03-15 18:45:32 -0400

    Update MyFileReader.java

--- BackEnd/src/MyFileReader.java
+++ BackEnd/src/MyFileReader.java
@@ -1,13 +1,12 @@
-
 /*
-*   Simple File Reader class 
-*   Reads through a file with a given file path
-*   Outputs each line of the file into a String ArrayList
-*
-*   Interacts with: Transactions.java
-*                   Accounts.Java
-*                   UserActivity.java
+*       MYFILEREADER CLASS
+* This class sends info and data from new/old items and account file
+* into three classes: Transactions, UserActivity and Accounts. 
+* Those three classes call MyFileReader for a certain database file 
+* and it reads in "currentFile", transfers it to the data ArrayList in order
+* to read in what is necessary. Then outputs each line of the file into a String ArrayList.
 */
+
 import java.io.*;
 import java.util.*;
 public class MyFileReader{
